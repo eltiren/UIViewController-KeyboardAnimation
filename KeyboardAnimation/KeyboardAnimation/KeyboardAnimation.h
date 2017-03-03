ff629ca4e77cf2eb951d9eee05a5d7fcb03498ea
Vitalii Yevtushenko <vitalii@rollncode.com>
2017-03-03 14:25:33 +0200

    add Xcode project with shared scheme

---
+++ KeyboardAnimation/KeyboardAnimation/KeyboardAnimation.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAnimation.h
+//  KeyboardAnimation
+//
+//  Created by Vitalii Yevtushenko on 3/3/17.
+//  Copyright © 2017 Roll'n'Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAnimation.
+FOUNDATION_EXPORT double KeyboardAnimationVersionNumber;
+
+//! Project version string for KeyboardAnimation.
+FOUNDATION_EXPORT const unsigned char KeyboardAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAnimation/PublicHeader.h>
+
+#import <KeyboardAnimation/UIViewController+KeyboardAnimation.h>
